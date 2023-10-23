e27acdac4ec609cd3b935328f1b489f7052a3711
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-10-22 22:21:28 -0500

    5 commit:arreglo de la estructura del codigo

--- ejvseis.c
+++ ejvseis.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int numero, digito, mayor = 0;
- // Leer el numero entero
+    // Leer el numero entero
     printf("Ingrese un numero entero:\n ");
     scanf("%d\n", &numero);
     // Obtener cada dígito del numero y determinar el mayor
-    while (numero != 0) {
-        digito = numero % 10;  // Obtener el ultimo digito del numero
-        if (digito > mayor) {
-            mayor = digito;  // Actualizar el valor del mayor digito encontrado
+    while (numero != 0)
+    {
+        digito = numero % 10; // Obtener el ultimo digito del numero
+        if (digito > mayor)
+        {
+            mayor = digito; // Actualizar el valor del mayor digito encontrado
         }
-        numero /= 10;  // Eliminar el ultimo digito del numero
+        numero /= 10; // Eliminar el ultimo digito del numero
     }
 
     // Mostrar el mayor dígito encontrado
     printf("El mayor digito del numero ingresado es: %d\n", mayor);
-    
-   
 
     return 0;
 }

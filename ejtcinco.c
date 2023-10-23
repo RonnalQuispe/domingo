e122b35ea266be885f13280218a0a386ad3f4d0c
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-10-22 22:29:53 -0500

    3 commit:obener el primer digito de cada numero

--- ejtcinco.c
+++ ejtcinco.c
@@ -3,12 +3,22 @@
 int main() {
     int num1, num2, primerDigito1, primerDigito2, productoMutuo;
 // Leer los dos números enteros
-    printf("Ingrese el primer número entero: ");
+    printf("Ingrese el primer numero entero: ");
     scanf("%d\n", &num1);
 
-    printf("Ingrese el segundo número entero: ");
+    printf("Ingrese el segundo numero entero: ");
     scanf("%d\n", &num2);
 
+    // Obtener el primer digito de cada numero
+    primerDigito1 = num1;
+    while (primerDigito1 >= 10) {
+        primerDigito1 /= 10;
+    }
+
+    primerDigito2 = num2;
+    while (primerDigito2 >= 10) {
+        primerDigito2 /= 10;
+    }
 
     
     return 0;

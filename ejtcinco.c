4aa39dd84c53d3384de279ae8a64c38ee97f21a1
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-10-22 22:27:55 -0500

    2 commit:leer numeros enteros

--- ejtcinco.c
+++ ejtcinco.c
@@ -2,6 +2,13 @@
 
 int main() {
     int num1, num2, primerDigito1, primerDigito2, productoMutuo;
+// Leer los dos números enteros
+    printf("Ingrese el primer número entero: ");
+    scanf("%d\n", &num1);
+
+    printf("Ingrese el segundo número entero: ");
+    scanf("%d\n", &num2);
+
 
     
     return 0;

8a2676a651a80af5363998bd50199e2f7b852412
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-10-22 21:10:16 -0500

    2 commit:declaracion de variable para e bucle

--- ejsiete.c
+++ ejsiete.c
@@ -1,6 +1,6 @@
 #include <stdio.h> //  Incluye la biblioteca estándar de entrada/salida para la función printf.
 
 int main(void){ //  Comienza la función principal.
-    
+    int l; // Declarar una variable entera 'l' para el bucle
     return 0; // Finaliza el programa devolviendo 0 desde la funcion principal
 }

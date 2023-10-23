4c30e42025b42ab0905eb761acbd1a670876b29a
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-10-22 21:07:59 -0500

    1 commit:estructura basica del programa

---
+++ ejsiete.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //  Incluye la biblioteca estándar de entrada/salida para la función printf.
+
+int main(void){ //  Comienza la función principal.
+    
+    return 0; // Finaliza el programa devolviendo 0 desde la funcion principal
+}

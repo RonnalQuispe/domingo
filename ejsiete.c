f2ed6b808e892d4caa114ae081b953b7a2564be2
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-10-22 21:12:35 -0500

    3  commit:bucle for desde 20 al 200 con inc. de 1

--- ejsiete.c
+++ ejsiete.c
@@ -2,5 +2,8 @@
 
 int main(void){ //  Comienza la función principal.
     int l; // Declarar una variable entera 'l' para el bucle
+     for (l = 20; l <= 200; l++)
+    { //  Inicia un bucle 'for' desde 20 hasta 200, incrementando en "l" 1 en cada iteración.
+    }
     return 0; // Finaliza el programa devolviendo 0 desde la funcion principal
 }

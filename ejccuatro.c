6d9938c3f38afb66684a29b51a5244be8f3d6aed
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-10-22 22:48:55 -0500

    5 commit:arreglar la estructura del codigo

--- ejccuatro.c
+++ ejccuatro.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int numero, factorial = 1;
- // Leer el numero
+    // Leer el numero
     printf("Ingrese un numero: ");
     scanf("%d\n", &numero);
     // Calcular el factorial
-    for (int i = 1; i <= numero; i++) {
+    for (int i = 1; i <= numero; i++)
+    {
         factorial *= i;
     }
     // Mostrar el resultado
     printf("El factorial de %d es: %d\n", numero, factorial);
 
-
-
-   
     return 0;
 }

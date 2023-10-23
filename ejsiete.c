960f184fed32e08ccbfe15900e2696534cbc769e
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-10-22 21:15:26 -0500

    5 commit:arreglo de estructura de programa

--- ejsiete.c
+++ ejsiete.c
@@ -1,11 +1,14 @@
 #include <stdio.h> //  Incluye la biblioteca estándar de entrada/salida para la función printf.
 
-int main(void){ //  Comienza la función principal.
+int main(void)
+{          //  Comienza la función principal.
     int l; // Declarar una variable entera 'l' para el bucle
-     for (l = 20; l <= 200; l++)
+    for (l = 20; l <= 200; l++)
     { //  Inicia un bucle 'for' desde 20 hasta 200, incrementando en "l" 1 en cada iteración.
-    if (l % 2 == 0){//  Comprueba si 'l' es par utilizando el operador módulo.
-            printf("%d, ", l);} //  Si 'l' es par, imprímelo seguido de una coma y un espacio.
+        if (l % 2 == 0)
+        { //  Comprueba si 'l' es par utilizando el operador módulo.
+            printf("%d, ", l);
+        } //  Si 'l' es par, imprímelo seguido de una coma y un espacio.
     }
     return 0; // Finaliza el programa devolviendo 0 desde la funcion principal
 }

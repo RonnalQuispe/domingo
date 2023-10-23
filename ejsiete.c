d423ca869df30001eafe2e61ab3741ec0a7179e3
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-10-22 21:14:34 -0500

    4 commit:condicion para buscar numero par e imprimirlo

--- ejsiete.c
+++ ejsiete.c
@@ -4,6 +4,8 @@ int main(void){ //  Comienza la función principal.
     int l; // Declarar una variable entera 'l' para el bucle
      for (l = 20; l <= 200; l++)
     { //  Inicia un bucle 'for' desde 20 hasta 200, incrementando en "l" 1 en cada iteración.
+    if (l % 2 == 0){//  Comprueba si 'l' es par utilizando el operador módulo.
+            printf("%d, ", l);} //  Si 'l' es par, imprímelo seguido de una coma y un espacio.
     }
     return 0; // Finaliza el programa devolviendo 0 desde la funcion principal
 }

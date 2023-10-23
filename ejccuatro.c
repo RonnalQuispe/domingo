b82190d363365d3282ba76f88eba17f79ef65a1c
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-10-22 22:45:21 -0500

    2 commit:calcula el factorial declarando la variable i que inicia desde 1>numero, y * el fac. por i y repetir el bucle

--- ejccuatro.c
+++ ejccuatro.c
@@ -5,6 +5,11 @@ int main() {
  // Leer el número
     printf("Ingrese un número: ");
     scanf("%d\n", &numero);
+    // Calcular el factorial
+    for (int i = 1; i <= numero; i++) {
+        factorial *= i;
+    }
+
 
 
    

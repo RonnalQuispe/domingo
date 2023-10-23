951ad392311aab3ed107531d03c5a9d0db59e449
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-10-22 21:36:32 -0500

    4 commit:imprimir x-y promedios

--- ejdsiete.c
+++ ejdsiete.c
@@ -7,9 +7,10 @@ int main(void){
 
     printf("Ingrese el valor de y: ");
     scanf("%d\n", &y);
-    smult2 = x * (x + 1); // Suma de los primeros x múltiplos de 2
-    smul5 = y * (y + 1) * 5; // Suma de los primeros y múltiplos de 5
-
-    return 0;// Finaliza el programa devolviendo 0 desde la función principal
+    smult2 = x * (x + 1); // Suma de los primeros x multiplos de 2
+    smul5 = y * (y + 1) * 5; // Suma de los primeros y multiplos de 5
+    printf("El promedio de los primeros %d múltiplos de 2 es: %.2f\n", x, prom2);
+    printf("El promedio de los primeros %d múltiplos de 5 es: %.2f\n", y, prom5);
+    return 0;// Finaliza el programa devolviendo 0 desde la funcion principal
 }
 
